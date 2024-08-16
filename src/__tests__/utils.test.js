f2623140d7d860b90a09ccff4d2e805edb6a7bf6
musin2 <muhsindaud75@gmail.com>
2024-08-16 23:24:08 +0300

    adds lab solution

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,8 +2,11 @@ import { currentAgeForBirthYear } from "../utils";
 
 describe("currentAgeForBirthYear", () => {
   it("returns the age of a person based on the year of birth", () => {
+    const currentYear = new Date().getFullYear();
     const birthYear = 1984;
     const ageOfPerson = currentAgeForBirthYear(birthYear);
-    expect(ageOfPerson).toBe(38);
+    const answer = currentYear - birthYear;
+
+        expect(ageOfPerson).toEqual(answer)
   });
 });
